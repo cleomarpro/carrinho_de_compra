97aefa6370e3cc85aba69c59d9ef9ed76007257b
cleomarpro <cleomar-pro@outlook.com>
2023-02-09 21:50:16 -0300

    En settings: registreis a apps criadas, produtos: foi criado a tabela Produto, admin: registrei a tabela Produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, default=0)
+    score = models.IntegerField()
+    image = models.ImageField( blank=True)
+def __str__(self): 
+        return str(self.name)+ ' - ' +str(self.price)
